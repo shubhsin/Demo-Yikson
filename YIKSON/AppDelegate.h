a1efdfdb1a0d41aca1028c99ce60af783524191a
shubhsin <luk4shubhs@gmail.com>
2016-02-10 22:54:32 +0530

    Initial Commit

---
+++ YIKSON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YIKSON
+//
+//  Created by Shubham Sorte on 10/02/16.
+//  Copyright © 2016 Shubham Sorte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YIKSON/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YIKSON
+//
+//  Created by Shubham Sorte on 10/02/16.
+//  Copyright © 2016 Shubham Sorte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
